858e459c2a165a6a282cb987e4257b058a975685
Brent <budden@gmail.com>
2020-06-08 17:26:29 -0500

    boostrap now symlinks deoldify rather than copying it

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/jantic/DeOldify.git deoldify_repo
-cp -r deoldify_repo/deoldify deoldify
+ln -s deoldify_repo/deoldify deoldify
 mkdir models
 wget https://www.dropbox.com/s/zkehq1uwahhbc2o/ColorizeArtistic_gen.pth?dl=0 -O ./models/ColorizeArtistic_gen.pth
 pip install -r requirements.txt
