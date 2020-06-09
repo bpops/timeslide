9ab4ea5e203211204265ac845aa7fcb8ed435a5d
Brent <budden@gmail.com>
2020-06-08 20:00:48 -0500

    can now enter url instead of opening local file. closes #2

--- timeslide.py
+++ timeslide.py
@@ -25,11 +25,13 @@ from tkinter import ttk
 from PIL import ImageTk, Image
 import shutil
 import os
+import urllib.request
+import io
 
 # canvas
 canv_width  = 500
 canv_height = 400
-bg_color    = "#ECEBEC"
+bg_color    = "#ECECEC"
 
 class Window(tk.Frame):
 
@@ -44,8 +46,11 @@ class Window(tk.Frame):
     # initialize window
     def init_window(self):
 
+        # load_method = (None, "open_file", "load_url")
+        self.load_method=None
+
         # setup
-        self.master.title("timeslide")
+        self.master.title("TimeSlide")
         self.pack(fill=tk.BOTH, expand=1)
 
         # creating a menu instance
@@ -65,23 +70,42 @@ class Window(tk.Frame):
             anchor='se')
         self.canvas.pack()
 
-        # button frame
-        btn_frame = tk.Frame(self, background=bg_color)
-        btn_frame.pack()
+        # FRAME - load old photo
 
+        frame_load = tk.LabelFrame(self, text="Step 1: Load Old Photo",
+            pady=4, bg=bg_color)
+        frame_load.pack(fill="x", padx=4)
+        
         # open_file button
-        btn_open_photo = ttk.Button(btn_frame, text="Load Photo",
+        btn_open_photo = ttk.Button(frame_load, text="Open Local File...",
             command=self.open_file)
         btn_open_photo.pack(side=tk.LEFT)
+        label_or = ttk.Label(frame_load, text="  or  ", background=bg_color)
+        label_or.pack(sid=tk.LEFT, padx=22)
+        
+        # load_url button
+        btn_load_url = ttk.Button(frame_load, text="Load URL",
+            command=self.load_url)
+        btn_load_url.pack(side=tk.RIGHT)
+        
+        # url_string
+        self.str_url = tk.Text(frame_load, width=40, height=1)
+        self.str_url.pack(side=tk.RIGHT, padx=4)
 
+        # FRAME - finish
+
+        frame_finish = tk.LabelFrame(self, text="Step 2: Finish Up",
+            pady=4, bg=bg_color)
+        frame_finish.pack(fill="x", padx=4)
+       
         # colorize button
-        self.btn_colorize = ttk.Button(btn_frame, text="Colorize!",
+        self.btn_colorize = ttk.Button(frame_finish, text="Slide Time!",
             command=self.colorize)
         self.btn_colorize['state'] = 'disabled'
         self.btn_colorize.pack(side=tk.LEFT)
 
         # save as button
-        self.btn_save_photo = ttk.Button(btn_frame, text="Save Photo",
+        self.btn_save_photo = ttk.Button(frame_finish, text="Save New Photo...",
             command=self.save_file)
         self.btn_save_photo['state'] = 'disabled'
         self.btn_save_photo.pack(side=tk.LEFT)
@@ -94,21 +118,47 @@ class Window(tk.Frame):
             ('Image files', '*.jpg *.jpeg *.png'),
         ]
         self.file_path = tk.filedialog.askopenfilename(filetypes=file_types)
+        
         # open image and resize
         img = Image.open(self.file_path)
         img = img.resize((canv_width, canv_height), Image.ANTIALIAS)
         self.canvas.img_tk = ImageTk.PhotoImage(img)
         self.canvas.itemconfig(self.image_id, image=self.canvas.img_tk)
 
-        # enable save button
+        # enable colorize button
         self.btn_colorize['state'] = 'normal'
+
+        # set load method
+        self.load_method="open_file"
+
+    # load from url
+    def load_url(self):
+        
+        # load raw data and display
+        raw_data = urllib.request.urlopen(self.str_url.get("1.0",tk.END)).read()
+        img = Image.open(io.BytesIO(raw_data))
+        img = img.resize((canv_width, canv_height), Image.ANTIALIAS)
+        self.canvas.img_tk = ImageTk.PhotoImage(img)
+        self.canvas.itemconfig(self.image_id, image=self.canvas.img_tk)
+
+        # enable colorize button
+        self.btn_colorize['state'] = 'normal'
+
+        # set load method
+        self.load_method="load_url"
 
     # colorize
     def colorize(self):
 
-        self.result_path = colorizer.plot_transformed_image(
-            path = self.file_path, render_factor = render_factor,
-            compare = False)
+        if self.load_method in "open_file":
+            self.result_path = colorizer.plot_transformed_image(
+                path = self.file_path, render_factor = render_factor,
+                compare = False)
+        elif self.load_method in "load_url":
+            print(self.str_url.get("1.0",tk.END))
+            self.result_path = colorizer.plot_transformed_image_from_url(
+                url=self.str_url.get("1.0",tk.END), path='//tmp/tmp.png',
+                render_factor = render_factor, compare = False)
         img = Image.open(self.result_path)
         img = img.resize((canv_width, canv_height), Image.ANTIALIAS)
         self.canvas.img_tk = ImageTk.PhotoImage(img)
@@ -133,7 +183,7 @@ class Window(tk.Frame):
 
 # configure primary window        
 root = tk.Tk()
-root.geometry("%ix434" % canv_width)
+root.geometry("%ix514" % canv_width)
 root.configure(bg=bg_color)
 
 # creation of an instance
