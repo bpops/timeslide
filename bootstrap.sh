30a8e5d5c6d216f4fd0aa56b6d030fcb95eab873
Brent <budden@gmail.com>
2020-06-13 20:23:15 -0500

    bootstrap now includes stable model, prepping for stable inclusion in models

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 git clone https://github.com/jantic/DeOldify.git deoldify_repo
 ln -s deoldify_repo/deoldify deoldify
 mkdir models
-wget https://www.dropbox.com/s/zkehq1uwahhbc2o/ColorizeArtistic_gen.pth?dl=0 -O ./models/ColorizeArtistic_gen.pth
+wget https://www.dropbox.com/s/zkehq1uwahhbc2o/ColorizeArtistic_gen.pth -O ./models/ColorizeArtistic_gen.pth
+wget https://www.dropbox.com/s/mwjep3vyqk5mkjc/ColorizeStable_gen.pth -O ./models/ColorizeStable_gen.pth
 pip install -r requirements.txt
+
