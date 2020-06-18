d32f934a46cb742884a655789c792ec4fc43cb1b
Brent <budden@gmail.com>
2020-06-17 20:17:07 -0500

    added isr and model get in bootstrap, readying super-resolution capability

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,14 @@
+
+# deoldify
 git clone https://github.com/jantic/DeOldify.git deoldify_repo
 ln -s deoldify_repo/deoldify deoldify
 mkdir models
 wget https://www.dropbox.com/s/zkehq1uwahhbc2o/ColorizeArtistic_gen.pth -O ./models/ColorizeArtistic_gen.pth
 wget https://www.dropbox.com/s/mwjep3vyqk5mkjc/ColorizeStable_gen.pth -O ./models/ColorizeStable_gen.pth
+
+# image-super-resolution
+git clone https://github.com/idealo/image-super-resolution.git isr_repo
+wget https://github.com/idealo/image-super-resolution/raw/master/weights/sample_weights/rdn-C6-D20-G64-G064-x2/PSNR-driven/rdn-C6-D20-G64-G064-x2_PSNR_epoch086.hdf5 -O ./models/
+
 pip install -r requirements.txt
 
