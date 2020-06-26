51ac5cf5b1e91deb7d93e20bcf24bddb625b7b1e
Brent <budden@gmail.com>
2020-06-25 21:48:27 -0500

    added logo/icon, closes #10

--- bundle.sh
+++ bundle.sh
@@ -1,6 +1,11 @@
 
 # timeslide macOS bundler
 
+# build using pyinstaller
 source ./venv/bin/activate
 rm -fr dist build
 pyinstaller timeslide.spec
+
+# convert icons, and bundle
+iconutil -c icns -o imgs/icon-windowed.icns imgs/icon.iconset
+mv imgs/icon-windowed.icns dist/timeslide.app/Contents/Resources/.
