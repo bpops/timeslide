e5d0299ca64ba54aa128d00b4c2a2ebedcf890c0
Brent <bpops@users.noreply.github.com>
2020-08-23 18:02:04 -0500

    updated bootstrap for python3, cleaned up

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,25 +23,8 @@ wget https://github.com/fannymonori/TF-LapSRN/raw/master/export/LapSRN_x2.pb -O 
 wget https://github.com/fannymonori/TF-LapSRN/raw/master/export/LapSRN_x4.pb -O ./models/LapSRN_x4.pb
 wget https://github.com/fannymonori/TF-LapSRN/raw/master/export/LapSRN_x8.pb -O ./models/LapSRN_x8.pb
 
-# dped
-# wget https://polybox.ethz.ch/index.php/s/7z5bHNg5r5a0g7k/download -O vgg_pretrained/imagenet-vgg-verydeep-19.mat
-
-# super-resolution repo
-#git clone https://github.com/krasserm/super-resolution.git
-#ln -s super-resolution/model model
-#ln -s super-resolution/data.py data.py
-#ln -s super-resolution/utils.py utils.py
-
-# super-resolution weights
-#wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xjyW_0dDS4jSTxKVZwtlkyfS4Oso-GUF' -O weights-edsr-16-x4.tar.gz
-#tar -xzf weights-edsr-16-x4.tar.gz
-#rm weights-edsr-16-x4.tar.gz
-#wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1JfQNGQZ9cG-lyC5EB0W3MpySjPlDJXpU' -O weights-wdsr-b-32-x4.tar.gz
-#tar -xzf weights-wdsr-b-32-x4.tar.gz
-#rm weights-wdsr-b-32-x4.tar.gz
-
 # python venv
 rm -fr venv
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
